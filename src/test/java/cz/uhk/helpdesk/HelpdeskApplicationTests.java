3debed8af1f562c85e329bc2b252486bc827ab50
Dominik Žilka <dominik.zilka@turnus.cz>
2020-02-09 16:08:22 +0100

    Initial commit.

---
+++ src/test/java/cz/uhk/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
